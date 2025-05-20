e03688ed1150b95bfd0ee56d62d383cf7ddd3f8d
MickeyJ <mickeyjmalotte@gmail.com>
2025-05-20 14:30:38 -0700

    add more paths to deploy config. try yml anchors

--- array-join-test.ts
+++
@@ -1,7 +0,0 @@
-const DIFFICULTY_LEVELS = {
-  easy: { pairs: 6, timeout: 1000 },
-  medium: { pairs: 8, timeout: 800 },
-  hard: { pairs: 12, timeout: 600 },
-};
-
-console.log(Object.values(DIFFICULTY_LEVELS));
