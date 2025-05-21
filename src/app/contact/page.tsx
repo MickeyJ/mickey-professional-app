198e71571aabbfe74f76c19075a525431ae3beb4
MickeyJ <mickeyjmalotte@gmail.com>
2025-05-21 13:25:44 -0700

    trying a deployment workflow refactor

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,9 @@
 export default function ContactPage() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-6 text-center">Contact</h1>
+      <p className="text-center">This is the contact page.</p>
+      <input type="text" />
+    </div>
+  );
 }
