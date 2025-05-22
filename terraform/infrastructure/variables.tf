119e4bbb8b689ed2e60672f1d48af804d90fe859
MickeyJ <mickeyjmalotte@gmail.com>
2025-05-21 20:13:25 -0700

    try iac and build-after-iac deploy again -

--- terraform/infrastructure/variables.tf
+++ terraform/infrastructure/variables.tf
@@ -45,6 +45,6 @@ variable "environment_variables" {
   description = "Environment variables for the Cloud Run service"
   type        = map(string)
   default = {
-    VERSION = "4.0.0"
+    VERSION = "5.0.0"
   }
 }
